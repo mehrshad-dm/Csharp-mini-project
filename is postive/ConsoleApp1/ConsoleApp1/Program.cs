f7c1c9511e713326ff3c096f13a66b8105ebef19
Mehrshad Doostmohammadi <152531328+mehrshad-dm@users.noreply.github.com>
2025-09-07 01:39:10 +0330

    Add files via upload

---
+++ is postive/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            Console.WriteLine("Welcome to the program to determine whether a number is postive or negative.");
+            Console.Write("Enter a number: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("Your number is postive.");
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("Your number is zero.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is negative.");
+            }
+        }
+    }
+}
