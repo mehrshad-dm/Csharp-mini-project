92842f5b43e7563a6569fc826add933c7819bd9e
Mehrshad Doostmohammadi <152531328+mehrshad-dm@users.noreply.github.com>
2025-09-07 01:40:54 +0330

    Add files via upload

---
+++ Mad Libs Game/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿string color, pluralNoun, celebrity;
+
+Console.Write("Enter your favorite color: ");
+color = Console.ReadLine();
+
+Console.Write("Enter a plural nonn: ");
+pluralNoun = Console.ReadLine();
+
+Console.Write("Enter your favorite celebrity: ");
+celebrity = Console.ReadLine();
+
+Console.WriteLine("Roses are " + color);
+Console.WriteLine(pluralNoun+" are blue");
+Console.WriteLine("I love "+ celebrity);
