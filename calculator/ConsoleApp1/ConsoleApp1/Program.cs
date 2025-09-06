b2b9d82718309b52e80c04b02dd6868d21211120
Mehrshad Doostmohammadi <152531328+mehrshad-dm@users.noreply.github.com>
2025-09-06 19:46:40 +0330

    Add files via upload

---
+++ calculator/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, Welcome to this app.");
+Console.Write("enter your first number:");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("enter your second number:");
+double number2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("the result is: ");
+Console.WriteLine(number1 + number2);
