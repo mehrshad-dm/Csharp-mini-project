4005a9eac12be9aeb35c75bbfa208cc87d0cb23d
Mehrshad Doostmohammadi <152531328+mehrshad-dm@users.noreply.github.com>
2025-09-07 01:37:18 +0330

    Delete calculator/ConsoleApp1 directory

--- calculator/ConsoleApp1/ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Hello, Welcome to this app.");
-Console.Write("enter your first number:");
-double number1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("enter your second number:");
-double number2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("the result is: ");
-Console.WriteLine(number1 + number2);
