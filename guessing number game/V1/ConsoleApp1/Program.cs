91b5bb2eb350c762452cc089c4f600283f67ad54
Mehrshad Doostmohammadi <152531328+mehrshad-dm@users.noreply.github.com>
2025-09-08 17:48:11 +0330

    Add files via upload

---
+++ guessing number game/V1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "hi";
+            string guess = "";
+
+            while (guess != secretWord)
+            {
+                Console.Write("Enter your guess: ");
+                guess = Console.ReadLine();
+                if (guess == secretWord)
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                } 
+                else 
+                {
+                    Console.WriteLine("You lose, Try again");
+                }
+            }
+        }
+    }
+}
---
+++ guessing number game/V2/V2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "hi";
+            string guess = "";
+            int times = 7;
+            while (times > 0)
+            {
+                Console.WriteLine("you have "+times+" chance");
+                Console.Write("enter your guess: ");
+                guess = Console.ReadLine();
+                times--;
+                if (guess == secretWord)
+                {
+                    Console.WriteLine("you win!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("you lose, try again, you have "+ times + " yet.\n");
+                }
+            }
+        }
+    }
+}
