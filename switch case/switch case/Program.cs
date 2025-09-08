d80967447f863841887896d642421646bcc14622
Mehrshad Doostmohammadi <152531328+mehrshad-dm@users.noreply.github.com>
2025-09-08 17:49:20 +0330

    Add files via upload

---
+++ switch case/switch case/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int DayNum = Convert.ToInt32(Console.ReadLine());
+            string res = GetDay(DayNum);
+            Console.WriteLine(res);
+        }
+        static string GetDay(int DayNum)
+            {
+            string dayName;
+            switch(DayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Number.";
+                    break;
+            }
+            return dayName;
+        }
+    }
+}
